1b829a6b97d427acbed2b1ba8349ace2b31ac613
김민석 <bjm7701@naver.com>
2022-01-07 12:37:28 +0900

    asdf

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, BackgroundTasks
 from typing import Optional
+import time
 from fastapi.responses import JSONResponse
 import numpy as np
 import asyncio
 from Ticker.Ticker import recv_ticker
 from CoinDataManager import coindata,label
-
-
+from fastapi.middleware.cors import CORSMiddleware
+origins = ["*"]
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -27,9 +35,13 @@ def on_app_shutdown():
 #BTCUSDT 같이 코인 정보를 넣으면 현재 서버에 있는 데이터를 로드해줌
 @app.get("/get/{coin}")
 def get_item(coin):
-    return JSONResponse({"data": coindata.price_data[label[coin.upper()]].tolist()})
+    return JSONResponse({"data": [time.time()*1000,coindata.price_data[label[coin.upper()]][-1]]})
 
 #BTCUSDT/BTCBNB 같이 넣으면 두값을 나눈 상대가격을 로드
 @app.get("/get_relative_price/{coin1}/{coin2}")
 def get_relative_price(coin1,coin2):
     return JSONResponse({"data": coindata.get_relative_price(coin1.upper(),coin2.upper()).tolist()})
+
+@app.get("/getlist/{coin}")
+def get_item(coin):
+    return JSONResponse({"data": coindata.price_data[label[coin.upper()]].tolist()})
