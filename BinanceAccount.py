a517706bd19ee01e37f977720eebc68fde839fdf
김민석 <bjm7701@naver.com>
2022-01-09 14:45:02 +0900

    바이낸스 API클래스 추가

--- BinanceAccount.py
+++ BinanceAccount.py
@@ -1,10 +1,17 @@
 import config
-
+import ccxt
+import binance
 class user:
     def __init__(self):
         self.APIKEY = config.get_secret('apiKey')
         self.SECRET = config.get_secret('secret')
-
-
+        self.binance = ccxt.binance({'apiKey' : self.APIKEY, 'secret' : self.SECRET})
+        self.balance = self.binance.fetch_balance()
+    def getBalance(self):
+        self.balance = self.binance.fetch_balance()
+        return self.balance
 if __name__ == '__main__':
     tmp = user()
+    balance = tmp.getBalance()
+    for k,v in balance.items():
+        print(k,v)
